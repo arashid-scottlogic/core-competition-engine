6f93a4d74757635c2bc0b6f5e3618d374bddc025
Giedre Stulgyte <gstulgyte@scottlogic.com>
2018-10-24 09:53:00 +0100

    sketching out the class

--- src/main/java/com/scottlogic/competition_engine/application/user/UserServiceImpl.java
+++ src/main/java/com/scottlogic/competition_engine/application/user/UserServiceImpl.java
@@ -1,13 +1,36 @@
 package com.scottlogic.competition_engine.application.user;
-
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.jsonwebtoken
+
+import javax.servlet.ServletException;
 
 @RestController
 public class UserServiceImpl implements UserService {
 
-    @RequestMapping(value = "/users/authorize")
-    public String authorizeTemp() {
-        return "Hello World";
+    @GetMapping(value = "/users/authorize")
+    public String authorizeTemp(String token) throws ServletException {
+
+        String username = "";
+
+        if (token==null) {
+            throw new ServletException("Empty token");
+        }
+
+
+
+        return username;
     }
+
+    // Returns decoded username
+    public String decodeUserToken(String token) {
+        try {
+            Claim claims =
+        } catch (Exception e) {
+            System.out.println("Invalid token");
+            return "";
+        }
+    }
+
+
 }
