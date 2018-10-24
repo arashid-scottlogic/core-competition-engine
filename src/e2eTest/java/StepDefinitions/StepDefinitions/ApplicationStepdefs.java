2453467d68aa2d58430d76db70d7a4e7a457b0da
Francis <fdonald@scottlogic.co.uk>
2018-10-24 15:50:41 +0100

    Added cucumber framework

---
+++ src/e2eTest/java/StepDefinitions/StepDefinitions/ApplicationStepdefs.java
@@ -0,0 +1,24 @@
+package StepDefinitions;
+
+import cucumber.api.java8.En;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ApplicationStepdefs implements En {
+    public ApplicationStepdefs() {
+        Given("^(true|false) is (true|false)$", (String arg1, String arg2) -> {
+            Boolean bool1 = Boolean.parseBoolean(arg1);
+            Boolean bool2 = Boolean.parseBoolean(arg2);
+            assertThat(bool1).isEqualTo(bool2);
+        });
+        When("^I ask if (true|false) is (true|false)$", (String arg1, String arg2) -> {
+            Boolean bool1 = Boolean.parseBoolean(arg1);
+            Boolean bool2 = Boolean.parseBoolean(arg2);
+            assertThat(bool1).isNotEqualTo(bool2);
+        });
+        Then("^I should receive (true|false)", (String arg1) -> {
+            Boolean bool1 = Boolean.parseBoolean(arg1);
+            assertThat(bool1).isEqualTo(false);
+        });
+    }
+}
