ae64a0c9657ff203579f8241782517610dbea85a
Giedre Stulgyte <gstulgyte@scottlogic.com>
2018-10-25 15:24:59 +0100

    added dependency

--- src/main/java/com/scottlogic/competitionEngine/application/user/UserServiceImpl.java
+++ src/main/java/com/scottlogic/competitionEngine/application/user/UserServiceImpl.java
@@ -1,14 +1,19 @@
 package com.scottlogic.competitionEngine.application.user;
 
+import com.sun.org.apache.xml.internal.security.algorithms.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.jsonwebtoken.security.Keys;
 import javax.servlet.ServletException;
+import java.security.Key;
 
 @RestController
 public class UserServiceImpl implements UserService {
+
+    public Key signingKey = Keys.secretKeyFor(SignatureAlgorithm.H256);
 
     @Autowired
     public UserServiceImpl() {}
