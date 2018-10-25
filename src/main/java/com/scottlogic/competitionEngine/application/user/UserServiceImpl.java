54909c779ba6ab0f81dbb52c1094e16ac9b69623
Giedre Stulgyte <gstulgyte@scottlogic.com>
2018-10-25 15:44:22 +0100

    minor tweaks + switch to junit5

--- src/main/java/com/scottlogic/competitionEngine/application/user/UserServiceImpl.java
+++ src/main/java/com/scottlogic/competitionEngine/application/user/UserServiceImpl.java
@@ -3,16 +3,18 @@ package com.scottlogic.competitionEngine.application.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class UserServiceImpl{
+@RequestMapping("/users")
+public class UserServiceImpl {
 
     @Autowired
     public UserServiceImpl() {}
 
-    @CrossOrigin
-    @GetMapping("/users/authorise")
+    @CrossOrigin (origins = "http://localhost:3000")
+    @GetMapping("/authorise")
     public String authoriseToken() {
 
         // for now it just returns a hardcoded String
