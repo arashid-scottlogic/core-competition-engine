18a1fd48ccfd21e6dead64fd7318f0ad1cc30fa7
Giedre Stulgyte <gstulgyte@scottlogic.com>
2018-10-25 14:45:57 +0100

    fixed unit test for endpoint

--- src/main/java/com/scottlogic/competition_engine/application/user/UserServiceImpl.java
+++ src/main/java/com/scottlogic/competition_engine/application/user/UserServiceImpl.java
@@ -1,17 +1,19 @@
 package com.scottlogic.competition_engine.application.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.ServletException;
-
 @RestController
 public class UserServiceImpl{
 
+    @Autowired
+    public UserServiceImpl() {}
+
     @CrossOrigin
-    @GetMapping(value="/users/authorise")
-    public String authoriseToken() throws ServletException {
+    @GetMapping("/users/authorise")
+    public String authoriseToken() {
 
         // for now it just returns a hardcoded String
         return "Hello World";
--- src/test/java/com/scottlogic/competition_engine/application/user/UserServiceImplTests.java
+++ src/test/java/com/scottlogic/competition_engine/application/user/UserServiceImplTests.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 
-import static org.hamcrest.CoreMatchers.endsWith;
+import static org.hamcrest.Matchers.endsWith;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -25,7 +25,7 @@ public class UserServiceImplTests {
     private MockMvc mockMvc;
 
     @Test
-    public void DefaultMessageReturned() throws Exception {
+    public void DefaultMessageReturned() throws Exception{
         this.mockMvc.perform(get("/users/authorise")).andDo(print()).andExpect(status().isOk())
                 .andExpect(content().string(endsWith("Hello World")));
     }
