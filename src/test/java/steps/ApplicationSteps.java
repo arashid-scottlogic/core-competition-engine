8ab56cd7069061c760e2b1735964c3e793b1b109
Francis <fdonald@scottlogic.co.uk>
2018-10-25 11:15:47 +0100

    Removed tests unrelated to framework setup

--- src/test/java/steps/ApplicationSteps.java
+++ src/test/java/steps/ApplicationSteps.java
@@ -1,24 +1,3 @@
 package steps;
 
 import cucumber.api.java8.En;
-
-import static org.assertj.core.api.Assertions.*;
-
-public class ApplicationSteps implements En {
-    public ApplicationSteps() {
-        Given("^(true|false) is (true|false)$", (String arg1, String arg2) -> {
-            Boolean bool1 = Boolean.parseBoolean(arg1);
-            Boolean bool2 = Boolean.parseBoolean(arg2);
-            assertThat(bool1).isEqualTo(bool2);
-        });
-        When("^I ask if (true|false) is (true|false)$", (String arg1, String arg2) -> {
-            Boolean bool1 = Boolean.parseBoolean(arg1);
-            Boolean bool2 = Boolean.parseBoolean(arg2);
-            assertThat(bool1).isNotEqualTo(bool2);
-        });
-        Then("^I should receive (true|false)", (String arg1) -> {
-            Boolean bool1 = Boolean.parseBoolean(arg1);
-            assertThat(bool1).isEqualTo(false);
-        });
-    }
-}
--- src/test/resources/com.scottlogic.features/Application.feature
+++
@@ -1,7 +0,0 @@
-Feature: Application Exists
-  Hasn't achieved sentience, yet.
-
-  Scenario: True isn't False
-    Given true is true
-    When I ask if true is false
-    Then I should receive false
